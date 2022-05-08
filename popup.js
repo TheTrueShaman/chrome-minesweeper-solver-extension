1d9915d1fac4317551652acd554523f3fdf61da4
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-07 22:02:43 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,3 @@
-async function getCurrentTab() {
-	let tab = await chrome.tabs.query({ active: true, currentWindow: true });
-	return tab;
-}
-
 let activatebutton = document.getElementById("activate");
 activatebutton.addEventListener("click", async () => {
 	let tab = await chrome.tabs.query({ active: true, currentWindow: true });
