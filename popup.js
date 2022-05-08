92aa2afdcf5c78141deabc9d8a904a0ef59b659a
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-07 22:51:13 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,4 +2,17 @@ let activatebutton = document.getElementById("activate");
 activatebutton.addEventListener("click", async () => {
 	let tab = await chrome.tabs.query({ active: true, currentWindow: true });
 	console.log(tab);
+	
+	let url = tab.url
+	if (url.slice(0,29) == 'https://www.google.com/search') {
+		if (url.includes('q=minesweeper&')) {
+			addsolver()
+		}
+	} else if (url == 'https://www.google.com/fbx?fbx=minesweeper') {
+		addsolver()
+	}
 });
+
+function addsolver() {
+	console.log('Correct url');
+}
