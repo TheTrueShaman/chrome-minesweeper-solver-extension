3919d1e0a70b8d54ffb79bde0277439a1a3b6399
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-07 22:13:27 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,11 +2,4 @@ let activatebutton = document.getElementById("activate");
 activatebutton.addEventListener("click", async () => {
 	let tab = await chrome.tabs.query({ active: true, currentWindow: true });
 	console.log(tab);
-	
-	/*
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: setPageBackgroundColor,
-	});
-	*/
 });
