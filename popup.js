e4b4eafaad7be26508359a094b50b024b7cc6573
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-07 21:56:51 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,7 +3,15 @@ async function getCurrentTab() {
 	return tab;
 }
 
-async function activate() {
-	let tab = await getCurrentTab();
+let activatebutton = document.getElementById("activate");
+activatebutton.addEventListener("click", async () => {
+	let tab = await chrome.tabs.query({ active: true, currentWindow: true });
 	console.log(tab);
-}
+	
+	/*
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: setPageBackgroundColor,
+	});
+	*/
+});
