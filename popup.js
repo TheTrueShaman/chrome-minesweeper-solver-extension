323f34a82ba7e984eca6e5d9af5ac5a02e3c10ed
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-15 14:40:25 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -28,7 +28,3 @@ function doSomething() {
 	document.getElementById('EYPL8e').innerHTML = '<span style="color:white; position: absolute;top: 10px; right: 110px; font-size:25px; cursor: pointer;" id="solve">&lt/></span>' + document.getElementById('EYPL8e').innerHTML;
 	document.getElementById('click', solve());
 }
-
-function solve() {
-	console.log('This is called');
-}
