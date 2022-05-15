cb74ec2aae338b45c139628266ae8008da6993bc
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-15 12:18:00 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -25,5 +25,5 @@ function doSomething() {
 	let board_controller = '[jscontroller=c20dae]';
 	let board = document.querySelector(board_controller);
 	console.log(board);
-	document.getElementById('EYPL8e').innerHTML = '<span style="color:white; position: absolute;top: 15px; right: 40px;">&lt/></span>' + document.getElementById('EYPL8e').innerHTML;
+	document.getElementById('EYPL8e').innerHTML = '<span style="color:white; position: absolute;top: 10px; right: 110px; font-size:25px">&lt/></span>' + document.getElementById('EYPL8e').innerHTML;
 }
