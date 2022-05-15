f7e2e09ae4de933935bfb713a637fb31cf460066
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-15 14:57:31 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -17,14 +17,6 @@ function addsolver(tab) {
 	console.log('Correct url');
 	chrome.scripting.executeScript({
 		target: { tabId: tab.id },
-		function: doSomething,
+		files: ['solve.js'],
 	});
-}
-
-function doSomething() {
-	let board_controller = '[jscontroller=c20dae]';
-	let board = document.querySelector(board_controller);
-	console.log(board);
-	document.getElementById('EYPL8e').innerHTML = '<span style="color:white; position: absolute;top: 10px; right: 110px; font-size:25px; cursor: pointer;" id="solve">&lt/></span>' + document.getElementById('EYPL8e').innerHTML;
-	document.getElementById('click', solve());
 }
