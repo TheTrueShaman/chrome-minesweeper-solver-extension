c1885bc3ac740a876944fd5547307157480ccef2
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-15 19:42:04 -0700

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,8 +1,7 @@
 function doSomething() {
 	let first_child = document.getElementById('EYPL8e').children[0];
 	let right = window.getComputedStyle(first_child).getPropertyValue('right');
-	console.log(right);
-	document.getElementById('EYPL8e').innerHTML = '<span style="color:white; position: absolute;top: 15px; right: 110px; font-size:25px; cursor: pointer;" id="solve">&lt/></span>' + document.getElementById('EYPL8e').innerHTML;
+	document.getElementById('EYPL8e').innerHTML = `<span style="color:white; position: absolute;top: 10px; right: ${right + 40}; font-size:25px; cursor: pointer;" id="solve">&lt/></span>` + document.getElementById('EYPL8e').innerHTML;
 	document.getElementById('solve').addEventListener('click', solve());
 }
 
@@ -10,7 +9,6 @@ function solve() {
 	let board_controller = '[jscontroller=c20dae]';
 	let board = document.querySelector(board_controller);
 	console.log(board);
-	console.log(board[1]);
 }
 
 doSomething();
