4cdc57d2469a1e7a1d0790197a9059296c87dbc4
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-15 19:38:21 -0700

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,5 +1,8 @@
 function doSomething() {
-	document.getElementById('EYPL8e').innerHTML = '<span style="color:white; position: absolute;top: 10px; right: 110px; font-size:25px; cursor: pointer;" id="solve">&lt/></span>' + document.getElementById('EYPL8e').innerHTML;
+	let first_child = document.getElementById('EYPL8e').children[0];
+	let right = window.getComputedStyle(first_child).getPropertyValue('right');
+	console.log(right);
+	document.getElementById('EYPL8e').innerHTML = '<span style="color:white; position: absolute;top: 15px; right: 110px; font-size:25px; cursor: pointer;" id="solve">&lt/></span>' + document.getElementById('EYPL8e').innerHTML;
 	document.getElementById('solve').addEventListener('click', solve());
 }
 
