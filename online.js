2c722a893fc651bc38e527f0332c8af8adb43e11
TheTrueShaman <68354378+TheTrueShaman@users.noreply.github.com>
2023-04-08 08:08:05 -0700

    Update online.js

--- online.js
+++ online.js
@@ -1,7 +1,11 @@
+if (window.location.href == "https://minesweeperonline.com") {
+	return;
+}
+
 function fetchBoard() {
-  // let game = document.getElementByID("game");
-  let tiles = document.getElementsByClassName("square");
-  console.log(tiles);
-  let x = document.getElementsByClassName("bordertb");
-  let y = tiles.length / x;
+	// let game = document.getElementByID("game");
+	let tiles = document.getElementsByClassName("square");
+	console.log(tiles);
+	let x = document.getElementsByClassName("bordertb");
+	let y = tiles.length / x;
 }
