563daee607234dadd6baa4bbeafc49379ff7bfc5
TheTrueShaman <68354378+TheTrueShaman@users.noreply.github.com>
2023-04-07 22:56:52 -0700

    Create online.js

---
+++ online.js
@@ -0,0 +1,7 @@
+function fetchBoard() {
+  // let game = document.getElementByID("game");
+  let tiles = document.getElementsByClassName("square");
+  console.log(tiles);
+  let x = document.getElementsByClassName("bordertb");
+  let y = tiles.length / x;
+}
