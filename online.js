cf48b5f0226ca086a19c4ab309678761a549910f
TheTrueShaman <68354378+TheTrueShaman@users.noreply.github.com>
2023-04-12 15:42:05 -0700

    Update online.js

--- online.js
+++ online.js
@@ -2,7 +2,7 @@ function fetchBoard() {
 	// let game = document.getElementByID("game");
 	let tiles = document.getElementsByClassName("square");
 	console.log(tiles);
-	let x = document.getElementsByClassName("bordertb");
+	let x = document.getElementsByClassName("bordertb").length / 3;
 	let y = tiles.length / x;
 }
 
