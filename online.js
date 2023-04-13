fc1c1b5cd74b6e76af9acca323a59f531a1a411e
TheTrueShaman <68354378+TheTrueShaman@users.noreply.github.com>
2023-04-12 17:38:36 -0700

    Update online.js

--- online.js
+++ online.js
@@ -3,7 +3,7 @@ function fetchBoard() {
 	let tiles = document.getElementsByClassName("square");
 	console.log(tiles);
 	let x = document.getElementsByClassName("bordertb").length / 3;
-	let y = tiles.length / x;
+	let y = (tiles.length - 2*(x+2)) / (x+2);
 }
 
 fetchBoard();
