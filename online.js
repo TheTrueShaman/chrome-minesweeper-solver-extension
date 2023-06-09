a56b593c244122b6184e52b8340b920c406d779f
TheTrueShaman <68354378+TheTrueShaman@users.noreply.github.com>
2023-06-09 13:59:21 -0700

    Delete Online.js
    I've decided to focus solely on Chrome's version of minesweeper

--- online.js
+++
@@ -1,9 +0,0 @@
-function fetchBoard() {
-	// let game = document.getElementByID("game");
-	let tiles = document.getElementsByClassName("square");
-	console.log(tiles);
-	let x = document.getElementsByClassName("bordertb").length / 3;
-	let y = (tiles.length - 2*(x+2)) / (x+2);
-}
-
-fetchBoard();
