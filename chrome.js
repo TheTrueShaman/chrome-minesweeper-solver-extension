36648f014f6341844414f21cc356ca0eb648cc42
TheTrueShaman <68354378+TheTrueShaman@users.noreply.github.com>
2023-12-03 14:44:59 -0800

    Update chrome.js

--- chrome.js
+++ chrome.js
@@ -28,27 +28,40 @@ function solve() {
 	let originalDimensions = {height: originalCanvas.height, width = originalCanvas.width};
 
 	//newCanvas is for drawing on
-	let newCanvas = '<canvas id="newCanvas" width="540" height="420" style="width: 540px; height: 420px; position: absolute; left: 0; z-index: 2; pointer-events: none;"></canvas>'
+	let newCanvas = '<canvas id="newCanvas" height="' + originalDimensions.height + '" width="' + originalDimensions.width +'" style="width: 540px; height: 420px; position: absolute; left: 0; z-index: 2; pointer-events: none;"></canvas>'
 	document.querySelector(canvas_name).insertAdjacentHTML("afterend", newCanvas);
 	newCanvas = document.getElementById('newCanvas');
 	newCTX = newCanvas.getContext("2d");
 
-	var x = 0;
-	var y = 0;
-	var squareX = 0;
-	var squareY = 0;
+	if (!sizes[originalDimensions.height]) {
+		console.error("Unrecognized board size");
+		return;
+	}
+	if (!sizes[originalDimensions.height] === originalDimensions.width) {
+		console.error("Unrecognized board size");
+		return;
+	}
+
+	let boardHeight = originalDimensions.height;
+	let boardWidth = originalDimensions.width;
+	let squareSize = sizes[originalDimensions.height]["square"];
+
+	let x = 0;
+	let y = 0;
+	let squareX = 0;
+	let squareY = 0;
 	document.addEventListener('mousemove', function (e) {
 		x = e.pageX - originalPos.x;
 		y = e.pageY - originalPos.y;
-		if ((x >= 0 && x < 540) && (y >= 0 && y < 420)) {
+		if ((x >= 0 && x < boardWidth) && (y >= 0 && y < boardHeight)) {
 			//console.log(`x: ${x}, y: ${y}`);
-			if ((Math.floor(x/30) != squareX) || (Math.floor(y/30) != squareY)) {
-				squareX = Math.floor(x/30);
-				squareY = Math.floor(y/30);
-				newCTX.clearRect(0, 0, 540, 420);
-				newCTX.fillStyle = "rgb(255,0, 0)";
-				newCTX.fillRect(squareX*30, squareY*30, 30, 30);
-				var data = originalCTX.getImageData(squareX*30 + 15, squareY*30 + 15, 1, 1).data;
+			if ((Math.floor(x/squareSize) != squareX) || (Math.floor(y/squareSize) != squareY)) {
+				squareX = Math.floor(x/squareSize);
+				squareY = Math.floor(y/squareSize);
+				newCTX.clearRect(0, 0, boardWidth, boardHeight);
+				newCTX.fillStyle = "rgb(255,0, 0)";				
+				newCTX.fillRect(squareX*squareSize, squareY*squareSize, squareSize, squareSize);
+				var data = originalCTX.getImageData(squareX*squareSize + squareSize/2, squareY*squareSize + squareSize/2, 1, 1).data;
 				console.log(`%crgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`, `color: rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255});`);
 			}
 		}
